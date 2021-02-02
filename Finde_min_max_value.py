9a06205d990b5ca7243682c4c62e8fa947e03aaf
Lev <mr.lmartirosyan888@gmail.com>
2021-02-02 12:28:16 +0400

    Finde_min_max_value

---
+++ Finde_min_max_value.py
@@ -0,0 +1,29 @@
+def min_max(a):
+    min = a[0]
+    max = a[0]
+    for i in a:
+        if i < min:
+            min = i
+        elif i > max:
+            max = i
+    return min,max            
+
+from random import randint
+
+
+
+
+b = []
+n = int(input('Enter size of list: '))
+for i in range(n):
+    b.append(randint(0,100))
+print(b)
+
+
+
+c = min_max(b)
+for i in range(len(c)):
+    if i == 0:
+        print('min value of list =',c[i])
+    else:
+        print('max value of list =',c[i])
